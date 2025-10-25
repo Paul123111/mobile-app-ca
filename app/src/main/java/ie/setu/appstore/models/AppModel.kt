6ac18de79744892b455c4f5171e14fa07463f31e
Paul Fitzgerald <116811799+Paul123111@users.noreply.github.com>
2025-10-25 16:23:28 +0100

    Feat/edit (#4)
    * Interacting with apps now passes value
    
    * Added edit functionality

--- app/src/main/java/ie/setu/appstore/models/AppModel.kt
+++ app/src/main/java/ie/setu/appstore/models/AppModel.kt
@@ -1,3 +1,8 @@
 package ie.setu.appstore.models
 
-data class AppModel(var name: String = "")
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class AppModel(var id: Long = 0,
+                    var name: String = "") : Parcelable

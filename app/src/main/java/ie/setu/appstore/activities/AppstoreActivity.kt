0750c71073d86134240c60958a15289f70ca7036
Paul123111 <20102629wit@gmail.com>
2025-10-25 10:32:56 +0100

    Add bottom nav bar

--- app/src/main/java/ie/setu/appstore/activities/AppstoreActivity.kt
+++ app/src/main/java/ie/setu/appstore/activities/AppstoreActivity.kt
@@ -1,11 +1,16 @@
 package ie.setu.appstore.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import ie.setu.appstore.R
 import ie.setu.appstore.databinding.ActivityAppstoreBinding
+import ie.setu.appstore.main.MainApp
+import timber.log.Timber.i
 
 class AppstoreActivity : AppCompatActivity() {
 
@@ -17,9 +22,9 @@ class AppstoreActivity : AppCompatActivity() {
         binding = ActivityAppstoreBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.toolbar.title = title
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
+//        binding.toolbar.title = title
+//        setSupportActionBar(binding.toolbar)
+//        supportActionBar?.setDisplayShowTitleEnabled(false)
 
     }
 
@@ -27,4 +32,20 @@ class AppstoreActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.bottom_hotbar, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_add -> {
+                val launcherIntent = Intent(this, AppstoreAddActivity::class.java)
+                getResult.launch(launcherIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+        }
 }
--- app/src/main/java/ie/setu/appstore/main/MainApp.kt
+++ app/src/main/java/ie/setu/appstore/main/MainApp.kt
@@ -1,14 +1,17 @@
 package ie.setu.appstore.main
 
 import android.app.Application
+import ie.setu.appstore.models.AppModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
+    var apps = ArrayList<AppModel>()
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         i("Appstore started")
+        apps.add(AppModel("testApp"))
     }
 }

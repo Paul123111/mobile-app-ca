6c3672472c91ce7c163d41126f900e11fd2dbef8
Paul Fitzgerald <116811799+Paul123111@users.noreply.github.com>
2025-12-24 14:26:42 +0000

    Merge feat/splash screen (#12)
    * Add custom splash screen
    
    * Change empty rating score to 0

--- app/src/main/java/ie/setu/appstore/models/AppModel.kt
+++ app/src/main/java/ie/setu/appstore/models/AppModel.kt
@@ -3,17 +3,13 @@ package ie.setu.appstore.models
 import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import kotlinx.serialization.Contextual
-import kotlinx.serialization.Serializable
 
 @Parcelize
-@Serializable
 data class AppModel(var id: Int = 0,
                     var name: String = "",
                     var appType: AppType = AppType.App,
                     var price: Int = 1,
                     var ratings: ArrayList<Int> = ArrayList(),
-                    @Contextual
                     var icon: Uri = Uri.EMPTY
 ) : Parcelable {
     enum class AppType(val value: Int) {App(0), Game(1)}
@@ -23,7 +19,7 @@ data class AppModel(var id: Int = 0,
     }
 
     fun avgRating(): Float {
-        if (ratings.isEmpty()) {return 3F}
+        if (ratings.isEmpty()) {return 0F}
         var sum = 0F
         for (rating in ratings) {
             sum += rating
